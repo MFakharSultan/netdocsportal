b05ef0781315d3999ab2bc3b3fa795221b68d890
Fakhar Sultan <fakharsultan.dev@gmail.com>
2025-07-02 23:02:48 +0500

    Fixed sidebar item

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
   integrations: [
     starlight({
       plugins: [starlightThemeRapide()],
-      title: "Netco",
+      title: "NetDocsPortal",
       // customCss: ["./src/styles/global.css"],
       sidebar: [
         {
